1e4d0716fdaeac6d8b75a35ae0c041b212c7824a
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-19 17:48:00 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
+# dog belongs to owner, has name, owner, mood
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = 'nervous'
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
